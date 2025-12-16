2e6d304ce0694a1a954685be4c97a520b63d25e1
mateooo2113 <mateo21toaquiza@gmail.com>
2025-12-16 11:33:09 -0500

    actualizacion holamundo2.sh

---
+++ holamundo2.sh
@@ -0,0 +1,12 @@
+Hola mundo en python 
+
+print ("Hola mundo")
+
+
+hola mundo en Java 
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
